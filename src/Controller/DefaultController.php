89bca00e5f3f6d1a71c6babaa5bf71467caf4369
Alex METAY <metayalex@orangefr>
2018-11-05 10:53:53 +0100

    default_controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,7 +13,7 @@ class DefaultController extends AbstractController
     public function index()
     {
         return $this->render('default/index.html.twig', [
-            'sd' => 'DefaultControle',
+            'controller_name' => 'DefaultControle',
         ]);
     }
 }
