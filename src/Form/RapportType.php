cfbc24d9832a303bda9b1530220106544a87d421
Alex METAY <metayalex@orangefr>
2018-11-08 14:46:13 +0100

    Version test - me0dicament

--- src/Form/RapportType.php
+++ src/Form/RapportType.php
@@ -5,8 +5,9 @@ namespace App\Form;
 use App\Entity\Medecin;
 use App\Entity\Medicament;
 use App\Entity\Visiteur;
-
 use App\Entity\Rapport;
+
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -33,8 +34,13 @@ class RapportType extends AbstractType
             ))
             ->add('idmedicament', EntityType::class, array(
                 'class' => Medicament::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('medicament')
+                        ->orderBy('medicament.nomcommercial', 'ASC');
+                },
                 'choice_label' => 'nomcommercial',
                 'label' => 'Nom du medicament',
+
             ))
         ;
     }
